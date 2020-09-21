19f772b04cd8aff805b83587759a719954283585
MontanaHarrison <66926252+MontanaHarrison@users.noreply.github.com>
2020-09-21 17:04:45 -0500

    Add files via upload

--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -7,12 +7,13 @@ class Inputs extends Component {
   return (
     <div key={this.props.itemHash} className="feature__item">
             <input
+              key={this.props.key}
               type="radio"
               id={this.props.itemHash}
               className="feature__option"
               name={this.props.slugify(this.props.feature)}
               checked={this.props.item.name === this.props.selected[this.props.feature].name}
-              onChange={(e) => this.props.update(e.target.name, this.props.item)}
+              onChange={(e) => this.props.update(this.props.feature, this.props.item)}
             />
             <label htmlFor={this.props.itemHash} className="feature__label">
               {this.props.item.name} ({this.props.USCurrencyFormat.format(this.props.item.cost)})
